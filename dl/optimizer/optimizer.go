4aebd4b85dc294cfa2193b97457d69ee24087a33
lipeng <734991033@qq.com>
2024-07-16 22:15:52 +0800

    optimizer:doc

--- dl/optimizer/optimizer.go
+++ dl/optimizer/optimizer.go
@@ -3,6 +3,11 @@ package optimizer
 // Optimizer 定义了优化器的方法
 type Optimizer interface {
 	//Update 方法用于更新网络参数
+	//params 和 grads。params 通常表示模型的参数（如权重和偏置）
+	//而 grads 表示这些参数相对于损失函数的梯度
 	Update(params []float64, grads []float64)
+
+	//学习率是一个超参数，它决定了每次更新时参数变化的幅度
+	//调整学习率对于控制训练过程中的收敛速度和稳定性至关重要
 	SetLearningRate(learningRate float64)
 }
