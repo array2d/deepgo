a7ae547a5acbbe814ee834a4d77fd35e452daa89
lipeng <734991033@qq.com>
2024-10-08 19:41:27 +0800

    relu:修复relu等的反向传播函数

--- dl/layer/activation.go
+++ dl/layer/activation.go
@@ -15,9 +15,23 @@ var Relu ActivationFunc = func(x float64) float64 {
 	return x
 }
 
+// ReluDerivative 计算ReLU的导数
+var ReluDerivative ActivationFunc = func(x float64) float64 {
+	if x > 0 {
+		return 1
+	}
+	return 0
+}
+
 // Sigmoid 实现Sigmoid激活函数
 var Sigmoid ActivationFunc = func(x float64) float64 {
 	return 1 / (1 + math.Exp(-x))
+}
+
+// SigmoidDerivative 计算Sigmoid的导数
+var SigmoidDerivative ActivationFunc = func(x float64) float64 {
+	s := Sigmoid(x)
+	return s * (1 - s)
 }
 
 // Tanh 实现Tanh激活函数
@@ -25,8 +39,14 @@ var Tanh ActivationFunc = func(x float64) float64 {
 	return math.Tanh(x)
 }
 
+// TanhDerivative 计算Tanh的导数
+var TanhDerivative ActivationFunc = func(x float64) float64 {
+	t := Tanh(x)
+	return 1 - t*t
+}
+
 // Activation 创建一个新的激活层
-func Activation(activationFunc ActivationFunc) (a *ComputeGraphNode) {
+func Activation(activationFunc, derivativeFunc ActivationFunc) (a *ComputeGraphNode) {
 	a = NewNode(nil, nil)
 	a.forward = func() {
 		input := a.Inputs[0].parameters["output"]
@@ -41,7 +61,7 @@ func Activation(activationFunc ActivationFunc) (a *ComputeGraphNode) {
 		input := a.Inputs[0].parameters["output"]
 		gradInput := input.Clone()
 		for i := range gradInput.Data {
-			gradInput.Data[i] = activationFunc(input.Data[i]) * gradOutput.Data[i]
+			gradInput.Data[i] = derivativeFunc(input.Data[i]) * gradOutput.Data[i]
 		}
 		a.Inputs[0].parameters["grad.output"] = gradInput
 	}
