99807f8556d739853e6dc2bdc013e68068f6b540
timeloveboy <734991033@qq.com>
2024-10-13 21:06:34 +0800

    windows:文件名不区分大小的bug

--- dl/tensor_op_A_B_return_C.py
+++ dl/tensor_op_A_B_return_C.py
@@ -1,24 +1,20 @@
 import numpy as np
 import sys
 import json
-
-def tensor_mul(a, b):
-    a = np.array(a)
-    b = np.array(b)
-    c = np.matmul(a, b)
-    return c.tolist()
-
+ 
 if __name__ == "__main__":
-    op=sys.argv[1]
-    a_data = json.loads(sys.argv[2])
-    a_shape = json.loads(sys.argv[3])
-    a = np.array(a_data).reshape(a_shape)
+    # 从命令行参数读取输入
+    op = sys.argv[1]
+    A_data = json.loads(sys.argv[2])
+    A_shape = json.loads(sys.argv[3])
+    A = np.array(A_data).reshape(A_shape)
 
     b_data = json.loads(sys.argv[4])
-    b_shape = json.loads(sys.argv[5])
-    b = np.array(b_data).reshape(b_shape)
-
-    if op == "mul":
-        c = tensor_mul(a, b)
-        print(np.array(c).flatten().tolist())  # 打印一维化的数据
-        print(np.array(c).shape)  # 换行打印 result 的 shape
+    b= tuple(b_data)
+    
+    if op == "sum":
+        # 计算结果
+        C = np.sum(A, b)
+        # 输出结果和形状
+        print(json.dumps(C.flatten().tolist()))
+        print(json.dumps(list(C.shape)))
